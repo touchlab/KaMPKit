9040aae16ac8a62d5335b9668a6e3751d5679db7
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-13 09:34:59 -0500

    Adding pod integration

--- sharedcode/build.gradle.kts
+++ sharedcode/build.gradle.kts
@@ -1,25 +1,9 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
     kotlin("multiplatform")
+    kotlin("native.cocoapods")
 }
 
 kotlin {
-    //select iOS target platform depending on the Xcode environment variables
-    val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-            ::iosArm64
-        else
-            ::iosX64
-
-    iOSTarget("ios") {
-        binaries {
-            framework {
-                baseName = "SharedCode"
-            }
-        }
-    }
-
     jvm("android")
 
     sourceSets["commonMain"].dependencies {
@@ -29,33 +13,9 @@ kotlin {
     sourceSets["androidMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
     }
-}
 
-val packForXcode by tasks.creating(Sync::class) {
-    val targetDir = File(buildDir, "xcode-frameworks")
-
-    /// selecting the right configuration for the iOS
-    /// framework depending on the environment
-    /// variables set by Xcode build
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val framework = kotlin.targets
-                          .getByName<KotlinNativeTarget>("ios")
-                          .binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-
-    from({ framework.outputDirectory })
-    into(targetDir)
-
-    /// generate a helpful ./gradlew wrapper with embedded Java path
-    doLast {
-        val gradlew = File(targetDir, "gradlew")
-        gradlew.writeText("#!/bin/bash\n"
-            + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
-            + "cd '${rootProject.rootDir}'\n"
-            + "./gradlew \$@\n")
-        gradlew.setExecutable(true)
+    cocoapods {
+        summary = "Lots of KaMP Stuff"
+        homepage = "https://github.com/touchlab/KaMPStarter"
     }
 }
-
-tasks.getByName("build").dependsOn(packForXcode)
