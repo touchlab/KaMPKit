4558442d29c55b33e56609d6e84fdf545b981ba5
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-13 11:19:50 -0500

    Adding buildSrc for Dependancy handling

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,3 @@
+object Versions {
+    const val ktor = "1.3.0-beta-2-SNAPSHOT"
+}
--- shared/src/commonMain/kotlin/common.kt
+++ shared/src/commonMain/kotlin/common.kt
@@ -5,3 +5,7 @@ expect fun platformName(): String
 fun createApplicationScreenMessage() : String {
     return "Kotlin Rocks on ${platformName()}"
 }
+
+fun todo(){
+
+}
