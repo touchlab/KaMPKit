dbb70f8578635fcd88cd9945db9b0116bf166223
Sam Hill <sam@touchlab.co>
2019-12-13 11:12:10 -0500

    Use kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     dependencies {
         classpath(Deps.android_gradle_plugin)
 
-        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+        classpath(kotlin("gradle-plugin", Versions.kotlin))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build gradle files
     }
