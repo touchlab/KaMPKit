57c80e61947fc4d00338e85fd03fb3b0df132b34
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-16 17:31:09 -0500

    Adding sqlDelight to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
     }
     dependencies {
         classpath(Deps.android_gradle_plugin)
+        classpath(Deps.SqlDelight.gradle)
 
         classpath(kotlin("gradle-plugin", Versions.kotlin))
         // NOTE: Do not place your application dependencies here; they belong
