4db1a7b92cb9403bf35b626e891197bf1eae7f32
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-18 13:34:46 -0500

    Replacting touchlab.com to touchlab.co

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdkVersion(Versions.compile_sdk)
     buildToolsVersion = Versions.buildToolsVersion
     defaultConfig {
-        applicationId = "com.touchlab.kampstarter"
+        applicationId = "co.touchlab.kampstarter"
         minSdkVersion(Versions.min_sdk)
         targetSdkVersion(Versions.target_sdk)
         versionCode = 1
