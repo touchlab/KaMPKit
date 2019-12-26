db6426cde3a22f7efa738b1c817bf8b4976ac48a
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-26 10:06:38 -0500

    Added more sql delight tests

--- shared/src/commonTest/kotlin/co/touchlab/kampstarter/SqlDelightTest.kt
+++ shared/src/commonTest/kotlin/co/touchlab/kampstarter/SqlDelightTest.kt
@@ -4,6 +4,7 @@ import com.squareup.sqldelight.db.SqlDriver
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 expect fun testDbConnection(): SqlDriver
 
@@ -16,12 +17,44 @@ abstract class SqlDelightTest {
     @BeforeTest
     fun setup(){
         dbHelper = DatabaseHelper(testDbConnection())
+        dbHelper.deleteAll()
+        dbHelper.insertBreed("Beagle")
     }
 
     @Test
-    fun `Get Boolean Setting Success`() = runTest {
-        dbHelper.insertBreed("Beagle")
+    fun `Select All Items Success`() = runTest {
         val breeds = dbHelper.selectAllItems().executeAsList()
-        assertNotNull(breeds.find { it.name == "Beagle"},"Could not retrieve Breed")
+        assertNotNull(breeds.find { it.name == "Beagle"},
+            "Could not retrieve Breed")
+    }
+
+    @Test
+    fun `Select Item by Id Success`() = runTest {
+        val breeds = dbHelper.selectAllItems().executeAsList()
+        val firstBreed = breeds.first()
+        assertNotNull(dbHelper.selectItemById(firstBreed.id).executeAsOneOrNull(),
+            "Could not retrieve Breed by Id")
+    }
+
+    @Test
+    fun `Update Favorite Success`() = runTest {
+        val breeds = dbHelper.selectAllItems().executeAsList()
+        val firstBreed = breeds.first()
+        dbHelper.updateFavorite(firstBreed.id,true)
+        val newBreed = dbHelper.selectItemById(firstBreed.id).executeAsOneOrNull()
+        assertNotNull(newBreed,
+            "Could not retrieve Breed by Id")
+        assertTrue(newBreed.favorite == 1L,
+            "Favorite Did Not Save")
+    }
+
+    @Test
+    fun `Delete All Success`() = runTest {
+        dbHelper.insertBreed("Poodle")
+        dbHelper.insertBreed("Schnauzer")
+        assertTrue(dbHelper.selectAllItems().executeAsList().isNotEmpty())
+        dbHelper.deleteAll()
+        assertTrue(dbHelper.selectAllItems().executeAsList().count() == 0,
+            "Delete All did not work")
     }
 }
