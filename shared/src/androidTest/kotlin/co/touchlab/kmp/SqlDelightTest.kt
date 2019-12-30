63fef87cff9d67221d2b702203849f6bfd1ebc41
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-30 16:26:25 -0500

    Renamed some test files

--- shared/src/androidTest/kotlin/co/touchlab/kmp/SqlDelightTest.kt
+++ shared/src/androidTest/kotlin/co/touchlab/kmp/SqlDelightTest.kt
@@ -5,22 +5,8 @@ import co.touchlab.kampstarter.db.KampstarterDb
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import com.squareup.sqldelight.db.SqlDriver
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.runBlocking
-import org.junit.runner.RunWith
-
-actual fun <T> runTest(block: suspend () -> T) { runBlocking { block() } }
 
 actual fun testDbConnection(): SqlDriver {
     val app = ApplicationProvider.getApplicationContext<Application>()
     return AndroidSqliteDriver(KampstarterDb.Schema, app, "droidcondb")
 }
-
-@RunWith(AndroidJUnit4::class)
-class SqlDelightTestJvm : SqlDelightTest()
-
-@RunWith(AndroidJUnit4::class)
-class KtorTestJvm: KtorTest()
-
-@RunWith(AndroidJUnit4::class)
-class BreedModelTestJvm: BreedModelTest()
--- shared/src/iosTest/kotlin/co/touchlab/kmp/SqlDelightTest.kt
+++ shared/src/iosTest/kotlin/co/touchlab/kmp/SqlDelightTest.kt
@@ -3,14 +3,5 @@ package co.touchlab.kampstarter
 import co.touchlab.kampstarter.db.KampstarterDb
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.ios.NativeSqliteDriver
-import kotlinx.coroutines.runBlocking
 
 actual fun testDbConnection(): SqlDriver = NativeSqliteDriver(KampstarterDb.Schema, "kampstarterdb")
-
-actual fun <T> runTest(block: suspend () -> T) { runBlocking { block() } }
-
-class SqlDelightTestJvm : SqlDelightTest()
-
-class KtorTestJvm: KtorTest()
-
-class BreedModelTestJvm: BreedModelTest()
