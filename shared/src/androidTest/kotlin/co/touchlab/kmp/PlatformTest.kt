892b09453f69d30287b84a109f79a242d79a7c31
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2019-12-30 16:26:25 -0500

    Renamed some test files

---
+++ shared/src/androidTest/kotlin/co/touchlab/kmp/PlatformTest.kt
@@ -0,0 +1,16 @@
+package co.touchlab.kampstarter
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import org.junit.runner.RunWith
+
+actual fun <T> runTest(block: suspend () -> T) { runBlocking { block() } }
+
+@RunWith(AndroidJUnit4::class)
+class SqlDelightTestJvm : SqlDelightTest()
+
+@RunWith(AndroidJUnit4::class)
+class KtorTestJvm: KtorTest()
+
+@RunWith(AndroidJUnit4::class)
+class BreedModelTestJvm: BreedModelTest()
