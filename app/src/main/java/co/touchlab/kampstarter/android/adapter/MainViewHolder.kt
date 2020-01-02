20eec76b6ee2883695b5eac10a6ae450ef16a715
Sam Hill <sam@touchlab.co>
2020-01-02 09:04:00 -0500

    VH formatting

--- app/src/main/java/co/touchlab/kampstarter/android/adapter/MainViewHolder.kt
+++ app/src/main/java/co/touchlab/kampstarter/android/adapter/MainViewHolder.kt
@@ -7,24 +7,20 @@ import androidx.recyclerview.widget.RecyclerView
 import co.touchlab.kampstarter.android.R
 import co.touchlab.kampstarter.db.Breed
 
-class MainViewHolder(itemView: View, breedClickListener: (Int)->Unit) : RecyclerView.ViewHolder(itemView) {
+class MainViewHolder(itemView: View, breedClickListener: (Int) -> Unit) : RecyclerView.ViewHolder(itemView) {
 
-    private val nameTextView = itemView.findViewById<TextView>(
-        R.id.breed_name_text_view
-    )
-    private val favoriteButton = itemView.findViewById<ImageButton>(
-        R.id.favorite_button
-    )
+    private val nameTextView = itemView.findViewById<TextView>(R.id.breed_name_text_view)
+    private val favoriteButton = itemView.findViewById<ImageButton>(R.id.favorite_button)
 
     init {
         favoriteButton.setOnClickListener { breedClickListener(adapterPosition) }
     }
 
-    fun bindTo(breed: Breed){
+    fun bindTo(breed: Breed) {
         nameTextView.text = breed.name
-        if(breed.favorite == 0L){
+        if (breed.favorite == 0L) {
             favoriteButton.setBackgroundResource(R.drawable.ic_favorite_border_24px)
-        }else{
+        } else {
             favoriteButton.setBackgroundResource(R.drawable.ic_favorite_24px)
         }
     }
