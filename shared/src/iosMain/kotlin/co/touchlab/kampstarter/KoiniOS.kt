a62ada5fdd19bc4bcce8f4277c6619408f82954a
russell <russell@touchlab.co>
2020-03-27 17:34:01 -0400

    Supply explicit delegates on Settings initialization
    I think this is a better model for how people integrating into existing projects will want things configured. They may have any arbitrary existing SharedPreferences or UserDefaults instance, so this makes explicit how that can be accessed from shared code.

--- shared/src/iosMain/kotlin/co/touchlab/kampstarter/KoiniOS.kt
+++ shared/src/iosMain/kotlin/co/touchlab/kampstarter/KoiniOS.kt
@@ -2,12 +2,17 @@ package co.touchlab.kampstarter
 
 import co.touchlab.kampstarter.db.KampstarterDb
 import com.russhwolf.settings.AppleSettings
+import com.russhwolf.settings.Settings
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.ios.NativeSqliteDriver
 import org.koin.dsl.module
+import platform.Foundation.NSUserDefaults
 
 fun initKoin() = initKoin{}
 actual val platformModule = module {
-    single { AppleSettings.Factory().create("KAMPSTARTER_SETTINGS") }
+    single<Settings> {
+        val userDefaults = NSUserDefaults(suiteName = "KAMPSTARTER_SETTINGS")
+        AppleSettings(userDefaults)
+    }
     single<SqlDriver> { NativeSqliteDriver(KampstarterDb.Schema, "kampstarterdb") }
 }
