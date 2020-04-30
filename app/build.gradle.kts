82e90b93c7bd906d4ec2b4e45104cdac40bb21b7
russell <russell@touchlab.co>
2020-04-30 17:08:42 -0400

    Dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 dependencies {
     implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
     implementation(project(":shared"))
-    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation(Deps.recyclerView)
     implementation(Deps.material_x)
     implementation(Deps.app_compat_x)
     implementation(Deps.core_ktx)
