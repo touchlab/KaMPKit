bc5feca3f960394d5cd9abb79e14534ff7271d3b
Kevin Schildhorn <kevins@touchlab.co>
2020-06-04 15:19:05 -0400

    Removing unused code

--- app/src/main/java/co/touchlab/kampstarter/android/MainActivity.kt
+++ app/src/main/java/co/touchlab/kampstarter/android/MainActivity.kt
@@ -58,23 +58,3 @@ class MainActivity : AppCompatActivity(), KoinComponent {
         viewModel.onDestroy()
     }
 }
-
-    internal class MainScope(private val mainContext: CoroutineContext, private val log: Kermit) : CoroutineScope {
-        override val coroutineContext: CoroutineContext
-            get() = mainContext + job + exceptionHandler
-
-        internal val job = SupervisorJob()
-        private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-            //printThrowable(throwable)
-            showError(throwable)
-        }
-
-        //TODO: Some way of exposing this to the caller without trapping a reference and freezing it.
-        private fun showError(t: Throwable) {
-            log.e(throwable = t) { "Error in MainScope" }
-        }
-
-        fun onDestroy(){
-            job.cancel()
-        }
-    }
