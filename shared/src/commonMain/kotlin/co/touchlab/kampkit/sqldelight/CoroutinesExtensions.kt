e1be7e83f0ce8ec2bf7e3b28f849e97b000ab795
Brady Aiello <brady.aiello@gmail.com>
2020-12-30 13:55:49 -0800

    Remove unused map<*>() extension functions.

--- shared/src/commonMain/kotlin/co/touchlab/kampkit/sqldelight/CoroutinesExtensions.kt
+++ shared/src/commonMain/kotlin/co/touchlab/kampkit/sqldelight/CoroutinesExtensions.kt
@@ -6,47 +6,9 @@ import com.squareup.sqldelight.TransactionWithoutReturn
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 import kotlin.jvm.JvmOverloads
-
-@JvmOverloads
-internal fun <T : Any> Flow<Query<T>>.mapToOne(
-    context: CoroutineContext = Dispatchers.Default
-): Flow<T> = map {
-    withContext(context) {
-        it.executeAsOne()
-    }
-}
-
-@JvmOverloads
-internal fun <T : Any> Flow<Query<T>>.mapToOneOrDefault(
-    defaultValue: T,
-    context: CoroutineContext = Dispatchers.Default
-): Flow<T> = map {
-    withContext(context) {
-        it.executeAsOneOrNull() ?: defaultValue
-    }
-}
-
-@JvmOverloads
-internal fun <T : Any> Flow<Query<T>>.mapToOneOrNull(
-    context: CoroutineContext = Dispatchers.Default
-): Flow<T?> = map {
-    withContext(context) {
-        it.executeAsOneOrNull()
-    }
-}
-
-@JvmOverloads
-internal fun <T : Any> Flow<Query<T>>.mapToOneNotNull(
-    context: CoroutineContext = Dispatchers.Default
-): Flow<T> = mapNotNull {
-    withContext(context) {
-        it.executeAsOneOrNull()
-    }
-}
 
 @JvmOverloads
 internal fun <T : Any> Flow<Query<T>>.mapToList(
