3a25336139abf90ce9b398f27e0741d5a010c4bf
Michael Friend <Mfriend@touchlab.co>
2021-01-21 17:34:21 -0500

    lint fixes

--- shared/src/commonTest/kotlin/co/touchlab/kampkit/mock/ClockMock.kt
+++ shared/src/commonTest/kotlin/co/touchlab/kampkit/mock/ClockMock.kt
@@ -3,6 +3,6 @@ package co.touchlab.kampkit.mock
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
-class ClockMock(var currentInstant: Instant = Clock.System.now()): Clock {
+class ClockMock(var currentInstant: Instant = Clock.System.now()) : Clock {
     override fun now(): Instant = currentInstant
 }
