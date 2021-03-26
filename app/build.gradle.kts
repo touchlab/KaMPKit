b0bcc6d0aa40a20ce4bc779d1cc0855e5c31b1ce
Brady Aiello <brady@touchlab.co>
2021-03-26 14:09:04 -0700

    Bringing Gradle and AGP back down per comments.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         }
     }
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
+        coreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
