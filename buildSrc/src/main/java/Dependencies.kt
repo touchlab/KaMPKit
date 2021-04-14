7c7f803c542407273a984f0829a520610f9a0657
russell <russell@touchlab.co>
2021-04-14 14:00:36 -0400

    Upgrade to Kotlin 1.5.0-RC

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     val target_sdk = 30
     val compile_sdk = 30
 
-    val kotlin = "1.4.30"
+    val kotlin = "1.5.0-RC"
     val android_gradle_plugin = "4.0.1"
 
     val buildToolsVersion = "30.0.3"
