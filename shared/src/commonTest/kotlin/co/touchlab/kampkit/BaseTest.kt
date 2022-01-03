c148342e21b0c2196915b070e9929de533366ce4
russell <russell@touchlab.co>
2022-01-03 17:07:09 -0500

    Remove custom runTest and use runBlocking or kotlinx runTest instead

--- shared/src/commonTest/kotlin/co/touchlab/kampkit/BaseTest.kt
+++ shared/src/commonTest/kotlin/co/touchlab/kampkit/BaseTest.kt
@@ -1,7 +1,3 @@
 package co.touchlab.kampkit
 
-import kotlinx.coroutines.CoroutineScope
-
-expect abstract class BaseTest() {
-    fun <T> runTest(block: suspend CoroutineScope.() -> T)
-}
+expect abstract class BaseTest()
