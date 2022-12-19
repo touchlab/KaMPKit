f9643b2599bca39078c712ae78a0f7fbdf8e4484
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-12-19 14:47:20 +0100

    Fix import order ktlint errors.

--- shared/src/androidTest/kotlin/co/touchlab/kampkit/TestUtilAndroid.kt
+++ shared/src/androidTest/kotlin/co/touchlab/kampkit/TestUtilAndroid.kt
@@ -2,10 +2,10 @@ package co.touchlab.kampkit
 
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
-import co.touchlab.kampkit.db.KaMPKitDb
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import co.touchlab.kampkit.db.KaMPKitDb
 
 internal actual fun testDbConnection(): SqlDriver {
     // Try to use the android driver (which only works if we're on robolectric).
