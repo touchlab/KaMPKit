af61b5abea5a1a5f3b85f83e93f3efdbd6f295cc
Jigar Brahmbhatt <findjigar@gmail.com>
2023-09-29 15:21:20 -0400

    Gradle + Kotlin + Dependency updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,15 +18,20 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
     lint {
         warningsAsErrors = false
         abortOnError = true
