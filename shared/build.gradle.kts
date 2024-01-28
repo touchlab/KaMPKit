d8a7a7b62cac9db07168501f8bf09e0aa7785cdc
Jigar Brahmbhatt <findjigar@gmail.com>
2024-01-28 10:48:00 +0530

    AGP update and minor refactoring

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -63,41 +63,31 @@ kotlin {
             }
         }
 
-        commonMain {
-            dependencies {
-                implementation(libs.koin.core)
-                implementation(libs.coroutines.core)
-                implementation(libs.sqlDelight.coroutinesExt)
-                implementation(libs.bundles.ktor.common)
-                implementation(libs.multiplatformSettings.common)
-                implementation(libs.kotlinx.dateTime)
-                implementation(libs.touchlab.skie.annotations)
-                api(libs.touchlab.kermit)
-            }
+        commonMain.dependencies {
+            implementation(libs.koin.core)
+            implementation(libs.coroutines.core)
+            implementation(libs.sqlDelight.coroutinesExt)
+            implementation(libs.bundles.ktor.common)
+            implementation(libs.multiplatformSettings.common)
+            implementation(libs.kotlinx.dateTime)
+            implementation(libs.touchlab.skie.annotations)
+            api(libs.touchlab.kermit)
         }
-        commonTest {
-            dependencies {
-                implementation(libs.bundles.shared.commonTest)
-            }
+        commonTest.dependencies {
+            implementation(libs.bundles.shared.commonTest)
         }
-        androidMain {
-            dependencies {
-                implementation(libs.androidx.lifecycle.viewmodel)
-                implementation(libs.sqlDelight.android)
-                implementation(libs.ktor.client.okHttp)
-            }
+        androidMain.dependencies {
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.sqlDelight.android)
+            implementation(libs.ktor.client.okHttp)
         }
-        val androidUnitTest by getting {
-            dependencies {
-                implementation(libs.bundles.shared.androidTest)
-            }
+        getByName("androidUnitTest").dependencies {
+            implementation(libs.bundles.shared.androidTest)
         }
-        iosMain {
-            dependencies {
-                implementation(libs.sqlDelight.native)
-                implementation(libs.ktor.client.ios)
-                api(libs.touchlab.kermit.simple)
-            }
+        iosMain.dependencies {
+            implementation(libs.sqlDelight.native)
+            implementation(libs.ktor.client.ios)
+            api(libs.touchlab.kermit.simple)
         }
     }
 
