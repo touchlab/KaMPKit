18cd36d80f8aef5a9d07330037eda2b3e8b310d2
Kevin Galligan <kgalligan@gmail.com>
2024-05-28 10:01:02 -0400

    Kotlin 2.0.0 and directly linking for iOS

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.skie) apply false
-    alias(libs.plugins.cocoapods) apply false
 }
 
 subprojects {
--- ios/Pods/Target Support Files/Pods-KaMPKitiOS/Pods-KaMPKitiOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KaMPKitiOS : NSObject
-@end
-@implementation PodsDummy_Pods_KaMPKitiOS
-@end
