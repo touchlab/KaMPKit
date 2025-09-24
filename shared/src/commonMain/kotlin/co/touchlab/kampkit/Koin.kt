cf7d5b9e6b1416e0c2721f7d4505f36846bc728a
Sam Hill <sam@touchlab.co>
2025-09-24 09:15:53 -0400

    Fix imports lint

--- shared/src/commonMain/kotlin/co/touchlab/kampkit/Koin.kt
+++ shared/src/commonMain/kotlin/co/touchlab/kampkit/Koin.kt
@@ -6,8 +6,8 @@ import co.touchlab.kampkit.models.BreedRepository
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.StaticConfig
 import co.touchlab.kermit.platformLogWriter
-import kotlinx.coroutines.Dispatchers
 import kotlin.time.Clock
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.KoinApplication
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
